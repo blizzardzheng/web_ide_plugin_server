dab04a08e8a5a936ba61b862d91abd961c67595f
Yang Zhen <yangzhen@coding.net>
2017-03-02 09:44:45 +0800

    Add content-type

--- src/main.js
+++ src/main.js
@@ -5,17 +5,29 @@ const cors = require('cors')
 const port = process.env.PORT || 8081  // SET OUR PORT
 import getPackageInfos from './getPackageInfos'
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:8060',
+  optionsSuccessStatus: 200,
+  credentials: true
+}
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.send('it works')
 })
 
 app.get('/packages', (req, res) => {
+  res.set(
+    'Content-Type', 'application/vnd.coding.v2+json;charset=UTF-8'
+  )
   res.json(getPackageInfos())
 })
 
 app.get('/packages/:pkgId', (req, res) => {
+  res.set(
+    'Content-Type', 'application/vnd.coding.v2+json;charset=UTF-8'
+  )
   res.json(getPackageInfos(req.params.pkgId))
 })
 
